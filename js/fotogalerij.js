b4cef27c89eed616f8354c85a511198c0b93056c
toon1992 <toon.detrue.v7134@student.hogent.be>
2016-10-21 12:32:05 +0200

    Toevoegen van fotos via folder via ajax

--- js/fotogalerij.js
+++ js/fotogalerij.js
@@ -2,6 +2,20 @@ var imagesPath =["img/CarRender.jpg",
                  "img/CarRender.jpg",
                  "img/CarRender.jpg"]
 
+var dir = "fotogalerij";
+
+$.ajax({
+  url:dir,
+  success:function(data){
+    $(data).find("a").attr("href", function(i,val){
+      if(val.match(/\.(jpe?g|png|gif)$/)){
+        $(".fotoGalerij").append($('<a>').attr('href',val).addClass('lightview').
+             append($('<img>').attr('src',val)));
+      }
+    })
+  }
+})
+
 /*var htmlString = "";
 
 $.each(imagesPath, function(i, item){
@@ -11,10 +25,15 @@ $.each(imagesPath, function(i, item){
 
 $("#fotoGalerij").append(htmlString);*/
 
-$(document).ready(function (){
-  $(".fotoGalerij").each(imagesPath, function(i,item){
-    $(this).append('<a href="' + item + '" class="lightview">').
-    append('<img src="' + item + '"alt="test"/>').
-    append('</a>');
+/*$(document).ready(function (){
+  var fotogalerij =  $(".fotoGalerij");
+$.each(imagesPath, function(i,item){
+   fotogalerij.append($('<a>').attr('href',item).addClass('lightview').
+        append($('<img>').attr('src',item)));
   });
-});
+});*/
+
+
+/*<$("#detail").append($("<ul>")
+                    .append($("<li>").addClass("detail detailHeader col-md-12 col-sm-12 col-xs-12")
+                            .append($("<div>").addClass("col-md-4 col-sm-4 col-xs-4").append($("<h1>")*/
